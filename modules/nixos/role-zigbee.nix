a0e44ed066314b1400d87aa293d4fe26a96ed307
David Guibert <david.guibert@gmail.com>
2024-06-04 22:12:01 +0200

    zigbee2mqtt: remove girier.js

--- modules/nixos/role-zigbee.nix
+++ modules/nixos/role-zigbee.nix
@@ -3,27 +3,6 @@
 let
   cfg = config.role.zigbee;
 
-  girier_js = pkgs.writeText "girier.js" ''
-    const tuya = require('zigbee-herdsman-converters/lib/tuya');
-    const reporting = require('zigbee-herdsman-converters/lib/reporting');
-
-    module.exports = [
-        {
-            fingerprint: [
-                {modelID: 'TS0001', manufacturerName: '_TZ3000_majwnphg'},
-                {modelID: 'TS0001', manufacturerName: '_TZ3000_6axxqqi2'},
-                {modelID: 'TS0001', manufacturerName: '_TZ3000_zw7yf6yk'},
-            ],
-            model: 'JR-ZDS01',
-            vendor: 'Girier',
-            description: '1 gang mini switch',
-            extend: tuya.extend.switch({switchType: true}),
-            configure: async (device, coordinatorEndpoint, logger) => {
-                await reporting.bind(device.getEndpoint(1), coordinatorEndpoint, ['genOnOff']);
-            },
-        },
-    ];
-  '';
 in
 {
   options.role.zigbee.enable = lib.mkOption {
@@ -45,9 +24,6 @@ in
       #includes = [
       #  config.secrets.zigbee2mqtt.secretFile
       #];
-      external_converters = [
-        girier_js
-      ];
       availability = {
         active.timeout = 10;
         passive.timeout = 10;
