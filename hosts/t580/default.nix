11a60ee431911162a1048956c0fc1ba03fac19f3
David Guibert <david.guibert@gmail.com>
2024-09-23 19:12:54 +0200

    t580: home-manager.users.dguibert

--- hosts/t580/default.nix
+++ hosts/t580/default.nix
@@ -5,7 +5,10 @@
     default = [ ];
   };
 
-  config.modules.hosts.t580 = [ ./configuration.nix ];
+  config.modules.hosts.t580 = [
+    ./configuration.nix
+    { home-manager.users.dguibert = { imports = config.modules.homes."dguibert@t580"; }; }
+  ];
 
   config.flake.nixosConfigurations = withSystem "x86_64-linux" ({ system, ... }: {
     t580 = inputs.self.lib.nixosSystem {
