90e07d71d0cf0f5d9a43aac1ad2987e8bf42fbce
David Guibert <david.guibert@gmail.com>
2024-11-05 17:31:53 +0100

    kanata: stick with qwerty key layout

--- modules/nixos/conf-kanata.nix
+++ modules/nixos/conf-kanata.nix
@@ -14,34 +14,34 @@ in
       enable = true;
       keyboards = {
         internalKeyboard = {
-          devices = [
-            "/dev/input/by-id/usb-0557_2419-event-kbd"
-            "/dev/input/by-id/usb-Dell_Dell_USB_Keyboard-event-kbd"
-          ];
+          #devices = [
+          #  "/dev/input/by-id/usb-0557_2419-event-kbd"
+          #  "/dev/input/by-id/usb-Dell_Dell_USB_Keyboard-event-kbd"
+          #];
           extraDefCfg = "process-unmapped-keys yes";
           config = ''
-            (defsrc
-             caps q s d f j k l m
-            )
             (defvar
-             tap-time 150
-             hold-time 200
+              tap-time 150
+              hold-time 200
             )
             (defalias
-             caps (tap-hold 100 100 esc lctl)
-             q (tap-hold $tap-time $hold-time a lmet)
-             s (tap-hold $tap-time $hold-time s lalt)
-             d (tap-hold $tap-time $hold-time d lsft)
-             f (tap-hold $tap-time $hold-time f lctl)
-             j (tap-hold $tap-time $hold-time j rctl)
-             k (tap-hold $tap-time $hold-time k rsft)
-             l (tap-hold $tap-time $hold-time l ralt)
-             m (tap-hold $tap-time $hold-time ; rmet)
-            )
+               caps (tap-hold 100 100 esc lctl)
+               a (tap-hold $tap-time $hold-time a lmet)
+               s (tap-hold $tap-time $hold-time s lalt)
+               d (tap-hold $tap-time $hold-time d lsft)
+               f (tap-hold $tap-time $hold-time f lctl)
+               j (tap-hold $tap-time $hold-time j rctl)
+               k (tap-hold $tap-time $hold-time k rsft)
+               l (tap-hold $tap-time $hold-time l ralt)
+               ; (tap-hold $tap-time $hold-time ; rmet)
+             )
 
-            (deflayer base
-             @caps @q  @s  @d  @f  @j  @k  @l  @m
-            )
+             (defsrc
+               caps a s d f j k l ;
+             )
+             (deflayer base
+               @caps @a  @s  @d  @f  @j  @k  @l  @;
+             )
           '';
         };
       };
