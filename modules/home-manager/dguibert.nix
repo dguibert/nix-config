a4317546d646e55415162f7f3a81d0f739d9c07d
David Guibert <david.guibert@gmail.com>
2024-11-11 21:16:37 +0100

    base16-vim: set target

--- modules/home-manager/dguibert.nix
+++ modules/home-manager/dguibert.nix
@@ -144,7 +144,7 @@ in
         programs.bash.initExtra = ''
           source ${config.lib.stylix.colors { templateRepo=inputs.base16-shell; use-ifd="always"; target = "base16"; }}
         '';
-        home.file.".vim/base16.vim".source = config.lib.stylix.colors { templateRepo = inputs.base16-vim; use-ifd = "always"; };
+        home.file.".vim/base16.vim".source = config.lib.stylix.colors { templateRepo = inputs.base16-vim; use-ifd = "always"; target="base16"; };
 
         xresources.properties = with config.lib.stylix.colors.withHashtag; {
           "*.faceSize" = config.stylix.fonts.sizes.terminal;
