35e89358e08752c16e89bdb910a765c32b314099
David Guibert <david.guibert@gmail.com>
2024-11-13 16:10:12 +0100

    hm: fix diffing with no old gen path

--- modules/home-manager/report-changes.nix
+++ modules/home-manager/report-changes.nix
@@ -4,8 +4,10 @@ with lib;
   options.home.report-changes.enable = mkEnableOption "report-changes";
   config = mkIf config.home.report-changes.enable {
     home.activation.report-changes = config.lib.dag.entryAnywhere ''
-      echo "Diffing: $oldGenPath $newGenPath"
-      ${activationPkgs.nix}/bin/nix store diff-closures $oldGenPath $newGenPath || true
+      if [[ -v oldGenPath ]]; then
+         echo "Diffing: $oldGenPath $newGenPath"
+         run ${activationPkgs.nix}/bin/nix store diff-closures $oldGenPath $newGenPath || true
+      fi
     '';
   };
 }
