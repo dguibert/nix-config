84c411b3ee02d39c10c96b043309c8c8eb6cbcce
David Guibert <david.guibert@gmail.com>
2025-01-05 15:18:42 +0100

    hm dguibert: expose options

--- modules/clan/home-manager/roles/dguibert.nix
+++ modules/clan/home-manager/roles/dguibert.nix
@@ -48,10 +48,17 @@ in
       };
     })
   ];
-  options = {
-    clan.home-manager.dguibert.withGui.enable = (mkEnableOption "Host running with X11 or Wayland") // { default = false; };
-    clan.home-manager.dguibert.withPersistence.enable = mkEnableOption "Use Impermanence";
-    clan.home-manager.dguibert.centralMailHost.enable = mkEnableOption "Host running liier/mbsync";
+  options.clan.home-manager.dguibert = {
+    withGui.enable = (mkEnableOption "Host running with X11 or Wayland") // { default = false; };
+    withPersistence.enable = mkEnableOption "Use Impermanence";
+    centralMailHost.enable = mkEnableOption "Host running liier/mbsync";
+    withEmacs.enable = lib.mkEnableOption "Enable emacs config";
+    withBash.enable = (lib.mkEnableOption "Enable bash config") // { default = true; };
+    withBash.history-merge = (lib.mkEnableOption "Enable bash history merging") // { default = true; };
+    withGpg.enable = (lib.mkEnableOption "Enable GPG config") // { default = true; };
+    withNix.enable = (lib.mkEnableOption "Enable nix config") // { default = true; };
+    withZellij.enable = (lib.mkEnableOption "Enable Zellij config"); # // { default = true; };
+    withVSCode.enable = (lib.mkEnableOption "Enable VSCode config"); # // { default = true; };
   };
 
   config = {
@@ -59,6 +66,15 @@ in
       imports = [
         inputs.sops-nix.homeManagerModules.sops
         inputs.impermanence.nixosModules.home-manager.impermanence
+        {
+          withEmacs.enable = cfg.withEmacs.enable;
+          withBash.enable = cfg.withBash.enable;
+          withBash.history-merge = cfg.withBash.history-merge;
+          withGpg.enable = cfg.withGpg.enable;
+          withNix.enable = cfg.withNix.enable;
+          withZellij.enable = cfg.withZellij.enable;
+          withVSCode.enable = cfg.withVSCode.enable;
+        }
         ({ config, lib, ... }: {
 
           config = lib.mkIf cfg.withPersistence.enable
