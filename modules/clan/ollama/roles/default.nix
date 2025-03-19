b10fdbd199f52f6bb33ac7621ccdd428604e3bc6
David Guibert <david.guibert@gmail.com>
2025-03-19 07:41:17 +0100

    ollama: avx2

--- modules/clan/ollama/roles/default.nix
+++ modules/clan/ollama/roles/default.nix
@@ -2,6 +2,7 @@
 {
   services.ollama = {
     enable = true;
+    environmentVariables.OLLAMA_LLM_LIBRARY = "cpu_avx2";
     #acceleration = "cuda";
   };
   #ollama run codellama:13b-instruct "Write an extended Python program with a typical structure. It should print the numbers 1 to 10 to standard output."
