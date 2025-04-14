e46349472c54178da856d49fc731f8e2cdc04195
David Guibert <david.guibert@gmail.com>
2025-04-14 20:20:59 +0200

    add oki printer

--- modules/clan/printing/roles/client.nix
+++ modules/clan/printing/roles/client.nix
@@ -9,4 +9,16 @@
     openFirewall = true;
   };
 
+  services.printing.enable = true;
+  hardware.printers = {
+    ensureDefaultPrinter = "OKI_MC363_C1E8FC";
+    ensurePrinters = [
+      {
+        deviceUri = "ipp://192.168.1.100/ipp";
+        location = "home";
+        name = "OKI_MC363_C1E8FC";
+        model = "everywhere";
+      }
+    ];
+  };
 }
