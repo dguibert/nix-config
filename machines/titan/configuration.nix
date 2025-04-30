f2cf0ef8804536f0149c55fffe9936d4f87e1002
David Guibert <david.guibert@gmail.com>
2025-04-30 11:36:33 +0200

    titan: enable memtest86

--- machines/titan/configuration.nix
+++ machines/titan/configuration.nix
@@ -149,6 +149,7 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 20;
   boot.loader.systemd-boot.consoleMode = "max";
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.timeout = 10;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi1";
