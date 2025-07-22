8fdaf6bbfa0dbef0e6b967ddbc6be2ebb9d2524b
David Guibert <david.guibert@gmail.com>
2025-07-22 15:32:09 +0200

    navidrome: set user

--- modules/clan-services/navidrome.nix
+++ modules/clan-services/navidrome.nix
@@ -29,10 +29,20 @@
           }:
           {
             services.navidrome.enable = true;
-            services.navidrome.openFirewall = true;
             services.navidrome.settings.Address = "192.168.1.24";
             services.navidrome.settings.Port = 4533;
-            services.navidrome.settings.MusicFolder = "/home/dguibert/Music/mopidy";
+            services.navidrome.settings.MusicFolder = "/home/dguibert/Music";
+
+            services.navidrome.user = "dguibert";
+            systemd.services.navidrome = {
+              serviceConfig.PrivateUsers = lib.mkForce false;
+              serviceConfig.PermissionsStartOnly = true;
+              unitConfig.RequiresMountsFor = "/home/dguibert/Music";
+            };
+            services.navidrome.openFirewall = true;
+            #networking.firewall.interfaces."bond0".allowedTCPPorts = [
+            #  config.services.navidrome.settings.Port
+            #];
           };
       };
   };
