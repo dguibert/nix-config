01e5928b6af4d81663ba4020fb92a461ab3581bf
David Guibert <david.guibert@gmail.com>
2025-08-17 19:34:52 +0200

    navidrome: jukebox

--- modules/clan-services/navidrome.nix
+++ modules/clan-services/navidrome.nix
@@ -32,6 +32,23 @@
             services.navidrome.settings.Address = "192.168.1.24";
             services.navidrome.settings.Port = 4533;
             services.navidrome.settings.MusicFolder = "/home/dguibert/Music";
+            services.navidrome.settings.Jukebox.Enabled = true;
+            services.navidrome.settings.MPVPath = "${pkgs.mpv}/bin/mpv";
+            # List of registered devices, syntax:
+            #  "symbolic name " - Symbolic name to be used in UI's
+            #  "device" - MPV audio device name, do mpv --audio-device=help to get a list
+
+            services.navidrome.settings.Jukebox.Devices = [
+              # "symbolic name " "device"
+              [
+                "auto"
+                "auto"
+              ]
+            ];
+
+            # Device to use for Jukebox mode, if there are multiple entries above.
+            # Using device "auto" if missing
+            #services.navidrome.settings.Jukebox.Default = "auto";
 
             services.navidrome.user = "dguibert";
             systemd.services.navidrome = {
