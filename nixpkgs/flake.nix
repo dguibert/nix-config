691fbaa09a0c1b60382bced092de33f691d9b118
David Guibert <david.guibert@gmail.com>
2025-10-13 18:15:46 +0200

    patch shadowsocks-libev

--- nixpkgs/flake.nix
+++ nixpkgs/flake.nix
@@ -15,12 +15,40 @@
           ];
           config.allowUnfree = true;
           config.allowUnsupportedSystem = true;
+          #config.permittedInsecurePackages = [
+          #  "mbedtls-2.28.10" # shadowsocks-libev
+          #];
         };
     in
     {
       lib = nixpkgs.lib;
 
-      overlays.default = final: prev: { };
+      overlays.default = final: prev: {
+        shadowsocks-libev = prev.shadowsocks-libev.overrideAttrs (o: with prev; {
+          buildInputs = [
+            libsodium
+            mbedtls
+            libev
+            c-ares
+            pcre
+          ];
+          nativeBuildInputs = with prev; [
+            cmake
+            asciidoc
+            xmlto
+            docbook_xml_dtd_45
+            docbook_xsl
+            libxslt
+          ];
+
+          patches = (o.patches or []) ++ [
+            (prev.fetchpatch {
+              url = "https://github.com/shadowsocks/shadowsocks-libev/commit/9afa3cacf947f910be46b69fc5a7a1fdd02fd5e6.patch";
+              hash = "sha256-rpWXe8f95UU1DjQpbKMVMwA6r5yGVaDHwH/iWxW7wcw=";
+            })
+          ];
+        });
+      };
 
       legacyPackages.x86_64-linux = nixpkgsFor "x86_64-linux";
       legacyPackages.x86_64-darwin = nixpkgsFor "x86_64-darwin";
