a776772cff86cdfd81d6b4b408f7c9abd5f3ccac
David Guibert <david.guibert@gmail.com>
2025-12-30 22:40:18 +0100

    3d_printing: new rotation distance

--- modules/clan-services/_3d_printing.nix
+++ modules/clan-services/_3d_printing.nix
@@ -213,7 +213,7 @@
                   # See calibrating rotation_distance on extruders doc
                   #rotation_distance = 21.54087;
                   #rotation_distance = "22.251425904873";
-                  rotation_distance = 4.6492;
+                  rotation_distance = 4.695;
                   #gear_ratio = "50:10"; # For Mini Afterburner
                   microsteps = 16;
                   nozzle_diameter = 0.400;
