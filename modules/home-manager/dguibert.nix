570721919090f9d9ca6cd994d15fbe5eb5b4fb61
David Guibert <david.guibert@gmail.com>
2026-01-04 22:59:38 +0100

    hm: add orca-sliver wrapped, freecad

--- modules/home-manager/dguibert.nix
+++ modules/home-manager/dguibert.nix
@@ -414,6 +414,21 @@ in
           type = "Application";
           mimeTypes = [ "x-scheme-handler/org-protocol" ];
         })
+
+        (pkgs.symlinkJoin {
+          name = "orca-slicer";
+          paths = [ orca-slicer ];
+          buildInputs = [ makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/orca-slicer \
+              --prefix LC_ALL : C \
+              --prefix MESA_LOADER_DRIVER_OVERRIDE : zink \
+              --prefix WEBKIT_DISABLE_DMABUF_RENDERER : 1 \
+              --prefix __EGL_VENDOR_LIBRARY_FILENAMES : ${mesa}/share/glvnd/egl_vendor.d/50_mesa.json \
+              --prefix GALLIUM_DRIVER : zink
+          '';
+        })
+        freecad-wayland
       ];
 
     # mimeapps.list
