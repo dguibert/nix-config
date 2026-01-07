d7d9d08ae8151a02ffb6b6507c1777a28fa6f79b
David Guibert <david.guibert@gmail.com>
2026-01-07 16:43:16 +0100

    seedbox[rtorrent]: flood static user

--- modules/clan-services/seedbox.nix
+++ modules/clan-services/seedbox.nix
@@ -257,6 +257,7 @@
           # allow access to the socket by putting it in the same group as rtorrent service
           # the socket will have g+w permissions
           systemd.services.flood.serviceConfig.SupplementaryGroups = [ config.services.rtorrent.group ];
+          systemd.services.flood.serviceConfig.DynamicUser = lib.mkForce false;
 
         };
     };
