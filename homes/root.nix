587ad662cd8555b0e7c4388e3d1aef8b861bed0b
David Guibert <david.guibert@gmail.com>
2026-02-28 22:13:46 +0100

    define root@rpi02 profile

--- homes/root.nix
+++ homes/root.nix
@@ -11,42 +11,32 @@ let
 in
 {
   imports = [
+    (genHomeManagerConfiguration "aarch64-linux" "root@rpi02")
     (genHomeManagerConfiguration "aarch64-linux" "root@rpi31")
     (genHomeManagerConfiguration "aarch64-linux" "root@rpi41")
     (genHomeManagerConfiguration "x86_64-linux" "root@t580")
     (genHomeManagerConfiguration "x86_64-linux" "root@titan")
   ];
 
+  modules.homes."root@rpi02" = [ { imports = [ ../modules/home-manager/root.nix ]; } ];
   modules.homes."root@rpi31" = [
-    (
-      { ... }:
-      {
-        imports = [ ../modules/home-manager/root.nix ];
-      }
-    )
+    {
+      imports = [ ../modules/home-manager/root.nix ];
+    }
   ];
   modules.homes."root@rpi41" = [
-    (
-      { ... }:
-      {
-        imports = [ ../modules/home-manager/root.nix ];
-      }
-    )
+    {
+      imports = [ ../modules/home-manager/root.nix ];
+    }
   ];
   modules.homes."root@t580" = [
-    (
-      { ... }:
-      {
-        imports = [ ../modules/home-manager/root.nix ];
-      }
-    )
+    {
+      imports = [ ../modules/home-manager/root.nix ];
+    }
   ];
   modules.homes."root@titan" = [
-    (
-      { ... }:
-      {
-        imports = [ ../modules/home-manager/root.nix ];
-      }
-    )
+    {
+      imports = [ ../modules/home-manager/root.nix ];
+    }
   ];
 }
