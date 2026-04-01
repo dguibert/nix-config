b19265e2d46ff07c40ed7bb1c95966db78b7c973
David Guibert <david.guibert@gmail.com>
2026-04-01 13:43:41 +0200

    titan: gtx 1030 supported by up to 580

--- machines/titan/configuration.nix
+++ machines/titan/configuration.nix
@@ -248,7 +248,7 @@ in
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     #package = config.boot.kernelPackages.nvidiaPackages.beta;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
     forceFullCompositionPipeline = true;
   };
 
