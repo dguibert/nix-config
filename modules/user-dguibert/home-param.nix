3a86adb8f6f53c19e7e8d9c0f111d39588cdb60d
David Guibert <david.guibert@gmail.com>
2026-05-28 07:57:34 +0200

    user-dguiberT.home-*: fix duplicate datalad

--- modules/user-dguibert/home-param.nix
+++ modules/user-dguibert/home-param.nix
@@ -16,67 +16,67 @@
   };
 
   configurations.home."gdavid@param-rudra".system = "x86_64-linux";
-  configurations.home."gdavid@param-rudra".module = {
-    imports = [
-      config.flake.modules.homeManager.custom_stdenv
-      config.flake.modules.homeManager.dguibert
-      config.flake.modules.homeManager.dguibert-bash
-      config.flake.modules.homeManager.dguibert-custom-profile
-      config.flake.modules.homeManager.dguibert-emacs
-      config.flake.modules.homeManager.dguibert-foot
-      config.flake.modules.homeManager.dguibert-git
-      config.flake.modules.homeManager.dguibert-gpg
-      config.flake.modules.homeManager.dguibert-htop
-      config.flake.modules.homeManager.dguibert-ssh
-      #config.flake.modules.homeManager.dguibert-tmux
-      config.flake.modules.homeManager.dguibert-zellij
-      config.flake.modules.homeManager.dguibert-annex
-    ];
-    home.username = "gdavid";
-    home.homeDirectory = "/home/gdavid";
-    home.stateVersion = "25.11";
+  configurations.home."gdavid@param-rudra".module =
+    { pkgs, ... }:
+    {
+      imports = [
+        config.flake.modules.homeManager.custom_stdenv
+        config.flake.modules.homeManager.dguibert
+        config.flake.modules.homeManager.dguibert-bash
+        config.flake.modules.homeManager.dguibert-custom-profile
+        config.flake.modules.homeManager.dguibert-emacs
+        config.flake.modules.homeManager.dguibert-foot
+        config.flake.modules.homeManager.dguibert-git
+        config.flake.modules.homeManager.dguibert-gpg
+        config.flake.modules.homeManager.dguibert-htop
+        config.flake.modules.homeManager.dguibert-ssh
+        #config.flake.modules.homeManager.dguibert-tmux
+        config.flake.modules.homeManager.dguibert-zellij
+        config.flake.modules.homeManager.dguibert-annex
+      ];
+      home.username = "gdavid";
+      home.homeDirectory = "/home/gdavid";
+      home.stateVersion = "25.11";
 
-    withCustomProfile.enable = true;
-    withCustomProfile.suffix = "";
+      withCustomProfile.enable = true;
+      withCustomProfile.suffix = "";
 
-    programs.bash.bashrcExtra = # (homes.withoutX11 args).programs.bash.initExtra +
-      ''
+      programs.bash.bashrcExtra = # (homes.withoutX11 args).programs.bash.initExtra +
+        ''
+          # support for x86_64/aarch64
+          # include .bashrc if it exists
+          [[ -f ~/.bashrc.$(uname -m) ]] && . ~/.bashrc.$(uname -m)
+        '';
+      programs.bash.profileExtra = ''
         # support for x86_64/aarch64
-        # include .bashrc if it exists
-        [[ -f ~/.bashrc.$(uname -m) ]] && . ~/.bashrc.$(uname -m)
+        # include .profile if it exists
+        [[ -f ~/.profile.$(uname -m) ]] && . ~/.profile.$(uname -m)
       '';
-    programs.bash.profileExtra = ''
-      # support for x86_64/aarch64
-      # include .profile if it exists
-      [[ -f ~/.profile.$(uname -m) ]] && . ~/.profile.$(uname -m)
-    '';
 
-    home.packages = with pkgs; [
-      nix
-      xpra
-      bashInteractive
+      home.packages = with pkgs; [
+        nix
+        xpra
+        bashInteractive
 
-      datalad
-      git-annex
-      git-nomad
-      mr
-      subversion
+        git-nomad
+        mr
+        subversion
 
-      tig
-      python3
-      python3Packages.pip
+        tig
+        python3
+        python3Packages.pip
 
-      nxsession
+        nxsession
 
-      figlet
-      fdupes
-      rdfind
-    ];
+        figlet
+        fdupes
+        rdfind
+      ];
 
-    home.sessionVariables.NIX_SSL_CERT_FILE = "/etc/pki/tls/certs/ca-bundle.crt";
-    home.sessionVariables.TMP = "/dev/shm";
+      home.sessionVariables.NIX_SSL_CERT_FILE = "/etc/pki/tls/certs/ca-bundle.crt";
+      home.sessionVariables.TMP = "/dev/shm";
 
-    programs.direnv.enable = true;
-    programs.direnv.nix-direnv.enable = true;
-  };
+      programs.direnv.enable = true;
+      programs.direnv.nix-direnv.enable = true;
+    };
 }
--- modules/user-dguibert/home-spartan.nix
+++ modules/user-dguibert/home-spartan.nix
@@ -22,58 +22,60 @@
   };
 
   configurations.home."bguibertd@spartan".system = "x86_64-linux";
-  configurations.home."bguibertd@spartan".module = {
-    imports = [
-      config.flake.modules.homeManager.custom_stdenv
-      config.flake.modules.homeManager.dguibert
-      config.flake.modules.homeManager.dguibert-bash
-      config.flake.modules.homeManager.dguibert-custom-profile
-    ];
-    home.username = "bguibertd";
-    home.homeDirectory = "/home_nfs/users/bguibertd";
-    home.stateVersion = "25.11";
+  configurations.home."bguibertd@spartan".module =
+    { pkgs, ... }:
+    {
+      imports = [
+        config.flake.modules.homeManager.custom_stdenv
+        config.flake.modules.homeManager.dguibert
+        config.flake.modules.homeManager.dguibert-bash
+        config.flake.modules.homeManager.dguibert-custom-profile
+      ];
+      home.username = "bguibertd";
+      home.homeDirectory = "/home_nfs/users/bguibertd";
+      home.stateVersion = "25.11";
 
-    withCustomProfile.enable = true;
-    withCustomProfile.suffix = "";
+      withCustomProfile.enable = true;
+      withCustomProfile.suffix = "";
 
-    programs.bash.enable = true;
-    programs.bash.historySize = -1; # no truncation
-    programs.bash.historyFile = "$HOME/.bash_history";
-    programs.bash.historyFileSize = -1; # no truncation
-    programs.bash.historyControl = [
-      "erasedups"
-      "ignoredups"
-      "ignorespace"
-    ];
-    programs.bash.historyIgnore = [
-      "ls"
-      "cd"
-      "clear"
-      "[bf]g"
-      " *"
-      "cd -"
-      "history"
-      "history -*"
-      "pwd"
-      "exit"
-      "date"
-    ];
+      programs.bash.enable = true;
+      programs.bash.historySize = -1; # no truncation
+      programs.bash.historyFile = "$HOME/.bash_history";
+      programs.bash.historyFileSize = -1; # no truncation
+      programs.bash.historyControl = [
+        "erasedups"
+        "ignoredups"
+        "ignorespace"
+      ];
+      programs.bash.historyIgnore = [
+        "ls"
+        "cd"
+        "clear"
+        "[bf]g"
+        " *"
+        "cd -"
+        "history"
+        "history -*"
+        "pwd"
+        "exit"
+        "date"
+      ];
 
-    programs.bash.bashrcExtra = # (homes.withoutX11 args).programs.bash.initExtra +
-      ''
+      programs.bash.bashrcExtra = # (homes.withoutX11 args).programs.bash.initExtra +
+        ''
+          # support for x86_64/aarch64
+          # include .bashrc if it exists
+          [[ -f ~/.bashrc.$(uname -m) ]] && . ~/.bashrc.$(uname -m)
+        '';
+      programs.bash.profileExtra = ''
         # support for x86_64/aarch64
-        # include .bashrc if it exists
-        [[ -f ~/.bashrc.$(uname -m) ]] && . ~/.bashrc.$(uname -m)
+        # include .profile if it exists
+        [[ -f ~/.profile.$(uname -m) ]] && . ~/.profile.$(uname -m)
       '';
-    programs.bash.profileExtra = ''
-      # support for x86_64/aarch64
-      # include .profile if it exists
-      [[ -f ~/.profile.$(uname -m) ]] && . ~/.profile.$(uname -m)
-    '';
-    home.sessionVariables.NIX_SSL_CERT_FILE = "/etc/pki/tls/certs/ca-bundle.crt";
-    home.sessionVariables.TMP = "/dev/shm";
+      home.sessionVariables.NIX_SSL_CERT_FILE = "/etc/pki/tls/certs/ca-bundle.crt";
+      home.sessionVariables.TMP = "/dev/shm";
 
-  };
+    };
 
   configurations.home."bguibertd@spartan-x86_64".system = "x86_64-linux";
   configurations.home."bguibertd@spartan-x86_64".module =
@@ -86,6 +88,7 @@
         config.flake.modules.homeManager.dguibert-emacs
         config.flake.modules.homeManager.dguibert-zellij
         config.flake.modules.homeManager.dguibert-custom-profile
+        config.flake.modules.homeManager.dguibert-annex
       ];
       home.username = "bguibertd";
       home.homeDirectory = "/home_nfs/users/bguibertd";
@@ -96,8 +99,6 @@
         xpra
         bashInteractive
 
-        datalad
-        git-annex
         git-nomad
         mr
         subversion
